15558e050a220ea43d3330492cf5545360585714
Hacktin <isues41@gmail.com>
2021-07-30 19:23:55 +0300

    Order(Aggregate Root and BaseEntity),OrderItem(BaseEntity) and Address(ObjectValue) have been created

---
+++ OrderSample/OrderSample.Domain/AggregateModels/OrderModels/Order.cs
@@ -0,0 +1,33 @@
+﻿using OrderSample.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSample.Domain.AggregateModels.OrderModels
+{
+    public class Order:BaseEntity,IAggregateRoot
+    {
+        public DateTime OrderDate { get; private set; }
+
+        public string Description { get; private set; }
+
+        public int BuyerId { get; private set; }
+
+        public string OrderStatus { get; private set; }
+
+        public Address Address { get; private set; }
+
+
+        public Order(DateTime orderDate,string description,int buyerId,string orderStatus,Address address)
+        {
+            this.OrderDate = orderDate;
+            this.Description = description;
+            this.BuyerId = buyerId;
+            this.OrderStatus = orderStatus;
+            this.Address = address;
+        }
+
+    }
+}
---
+++ OrderSample/OrderSample.Domain/AggregateModels/OrderModels/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using OrderSample.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSample.Domain.AggregateModels.OrderModels
+{
+    public class OrderItem:BaseEntity
+    {
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
