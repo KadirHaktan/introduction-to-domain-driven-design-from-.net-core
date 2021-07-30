6061875f7e15e14d0ffc7a8c6a404d2823db694a
Hacktin <isues41@gmail.com>
2021-07-30 15:28:26 +0300

    other all class libraries's class 1 files have been remove

--- OrderSample/OrderSample.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OrderSample.Domain
-{
-    public class Class1
-    {
-    }
-}
--- OrderSample/OrderSample.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OrderSample.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
