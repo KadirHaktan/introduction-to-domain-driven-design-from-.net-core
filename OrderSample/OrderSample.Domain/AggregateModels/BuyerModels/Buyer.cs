c67c5c5a0b7512c9d40e96804778170596dc6b0f
Hacktin <isues41@gmail.com>
2021-07-30 19:23:12 +0300

    Buyer Model has been created

---
+++ OrderSample/OrderSample.Domain/AggregateModels/BuyerModels/Buyer.cs
@@ -0,0 +1,16 @@
+﻿using OrderSample.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSample.Domain.AggregateModels.BuyerModels
+{
+    public class Buyer:BaseEntity
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
